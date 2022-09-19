51e305f2c077e61191220a97012757fd608df4a6
FatihKilit <fatih.kilit@evatro.com>
2022-09-19 15:31:35 +0300

    Project created

---
+++ FoodInBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodInBox
+//
+//  Created by Fatih Kilit on 19.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
