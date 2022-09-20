98bf93990844b49ae8adcbfdf33effcbba8e0af6
FatihKilit <fatih.kilit@evatro.com>
2022-09-20 20:25:07 +0300

    sort algorithm has been added for products.

--- FoodInBox/Core/Components/ViewControllers/Products/ProductsViewModel.swift
+++ FoodInBox/Core/Components/ViewControllers/Products/ProductsViewModel.swift
@@ -28,6 +28,17 @@ class ProductsViewModel {
                         tempProducts.append(product)
                     }
                 }
+                tempProducts.sort { p1, p2 in
+                    if p1.campaignPrice != nil {
+                        return (p1.campaignPrice ?? 0) < (p2.price ?? 0)
+                    } else if p2.campaignPrice != nil {
+                        return (p1.price ?? 0) < (p2.campaignPrice ?? 0)
+                    } else if p1.campaignPrice != nil && p2.campaignPrice != nil {
+                        return (p1.campaignPrice ?? 0) < (p2.campaignPrice ?? 0)
+                    }
+                    
+                    return (p1.price ?? 0) < (p2.price ?? 0)
+                }
                 self.products.value = tempProducts
             }
         }
